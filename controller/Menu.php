f97c0b2476e42ebe166526ba43ddf7cecc79510e
SharleneYu <sharleneyu@hotmail.com>
2023-09-08 20:38:23 +0800

    完成P35十二選單管理功能中主選單CRUD，但未完成次選單、前台顯示

--- controller/Menu.php
+++ controller/Menu.php
@@ -9,12 +9,23 @@ class Menu extends DB{
     }
 
     function backend(){
+        // 先撈主選單main_id=0
+        $rows=$this->all(['main_id'=>0]);
+        // 再撈每個主選單的次選單數，存到$row['subs]、再塞到$rows
+        foreach($rows as $idx=>$row){
+            $row['subs']=$this->count(['main_id'=>$row['id']]);
+            $rows[$idx]=$row;
+        }
+
         $view=[
             'header'=>'選單管理',
             'table'=>$this->table,
-            'rows'=>$this->all(),
+            'rows'=>$rows,
             'addBtn'=>'新增主選單',
-            'modal'=>"./view/modal/menu.php"          
+            'modal'=>"./view/modal/menu.php",   
+            'updateModal'=>"./view/modal/subMenu.php",
+            'updateBtn'=>"編輯次選單"     
+            
         ];
         return $this->view('./view/backend/menu.php', $view);
     }
---
+++ view/backend/menu.php
@@ -0,0 +1,55 @@
+<div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
+    <p class="t cent botli">選單管理</p>
+    <form method="post" action="./api/update.php">
+        <table width="100%">
+            <tbody>
+                <tr class="yel">
+                    <td width="30%">主選單名稱</td>
+                    <td width="30%">選單連結網址</td>
+                    <td width="20%">次選單數</td>
+                    <td width="10%">顯示</td>
+                    <td width="10%">刪除</td>
+                    <td></td>
+                </tr>
+                <?php
+                foreach($rows as $row){
+                ?>
+                <tr>
+                    <td width="30%">
+                        <input type="text" name="text[<?=$row['id'];?>]" value="<?=$row['text'];?>">
+                    </td>
+                    <td width="30%">
+                        <input type="text" name="href[<?=$row['id'];?>]" value="<?=$row['href'];?>">
+                    </td>
+                    <td width="20%">
+                        <?=$row['subs']?>
+                    </td>
+                    <td width="10%">
+                        <input type="checkbox" name="sh[<?=$row['id'];?>]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>>
+                    </td>
+                    <td>
+                        <input type="checkbox" name="del[<?=$row['id'];?>]" value="<?=$row['id'];?>">
+                    </td>
+                    <td width="10%">
+                        <input type="button" value="<?=$updateBtn;?>" onclick="op('#cover','#cvr','<?=$updateModal;?>?id=<?=$row['id'];?>')">
+                    </td>
+                </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+
+
+        <table style="margin-top:40px; width:70%;">
+            <tbody>
+                <tr>
+                    <input type="hidden" name="table" value='<?=$table?>'>
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','<?=$modal;?>')" value="<?=$addBtn;?>"></td>
+                    <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
+                </tr>
+            </tbody>
+        </table>
+
+    </form>
+</div>
